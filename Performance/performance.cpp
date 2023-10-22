95cef896ca8c228640c1ef3cf7532264785e6e78
bhatnitish1998 <bhatnitish1998@gmail.com>
2023-10-22 12:33:26 +0530

    changed success,error,timeout rates to percentages

--- Performance/performance.cpp
+++ Performance/performance.cpp
@@ -6,47 +6,48 @@
 #include <chrono>
 #include <sstream>
 
-using namespace std;
 
 // Currently lot of things are done by bash scripts
 class PerfAnalyzer
 {
-    string num_of_clients;
-    string iterations;
-    string sleep_time;
-    string timeout_time;
+    std::string num_of_clients;
+    std::string iterations;
+    std::string sleep_time;
+    std::string timeout_time;
     double total_time;
 
 public:
     void cleanup()
     {
-        string cmd2 = "bash cleanup.sh";
+        std::string cmd2 = "bash cleanup.sh";
         system(cmd2.c_str());
     }
     void run_load_test()
     {
-        string cmd1 = "bash loadtest.sh " + num_of_clients + " " + iterations + " " + sleep_time + " " + timeout_time;
-        auto start_time = chrono::high_resolution_clock::now();
+        std::string cmd1 = "bash loadtest.sh " + num_of_clients + " " + iterations + " " + sleep_time + " " + timeout_time;
+        auto start_time = std::chrono::high_resolution_clock::now();
         system(cmd1.c_str());
-        auto end_time = chrono::high_resolution_clock::now();
-        total_time = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
+        auto end_time = std::chrono::high_resolution_clock::now();
+        total_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
     }
     void display_metrics()
     {
         // Uses file as of now to read from generated bash script
 
-        ifstream fin("global_data.txt");
+        std::ifstream fin("global_data.txt");
         double success, average_response, requests, timeouts;
 
         fin >> average_response >> requests >> success >> timeouts;
         fin.close();
 
-        cout << "Throughput = " << (success / total_time) * 1000 << endl;
-        cout << "Average response time = " << average_response << endl;
-        cout << "Success rate = " << (success / total_time) * 1000 << endl;
-        cout << "Request rate = " << ((requests / total_time)) * 1000 << endl;
-        cout << "Error rate = " << ((requests - success) / total_time) * 1000 << endl;
-        cout << "Number of clients = " << num_of_clients << endl;
+        std::cout << "Number of clients = " << num_of_clients << std::endl;
+        std::cout << "Throughput = " << (success / total_time) * 1000 << std::endl;
+        std::cout << "Average response time = " << average_response << std::endl;
+        std::cout << "Request rate = " << ((requests / total_time)) * 1000 << std::endl;
+        std::cout << "Success percent = " << (success / requests) * 100 << std::endl;
+        std::cout << "Timeout percent = " << (timeouts/requests)*100 <<std::endl;
+        std::cout << "Error percent = " << ((requests - success) / requests) * 100 << std::endl;
+
     }
     PerfAnalyzer(const char *num_of_clients, const char *iterations, const char *sleep_time, const char *timeout_time)
         : num_of_clients(num_of_clients), iterations(iterations), sleep_time(sleep_time), timeout_time(timeout_time), total_time(0)
