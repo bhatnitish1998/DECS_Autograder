79e76f9bb4294a49fa04cc38c7916e3612ea226f
bhatnitish1998 <bhatnitish1998@gmail.com>
2023-11-29 02:50:10 +0530

    added Version 3 server

---
+++ Ver_3/Server.cpp
@@ -0,0 +1,80 @@
+#include "Server.hpp"
+
+Server::Server(const char *port,const char *pool_size) : port(std::stoi(port)),pool_size(std::stoi(pool_size))
+{
+    backlog = 5;
+    setup_threadpool();
+    setup_socket();
+}
+
+void Server::setup_socket()
+{
+    int status;
+    addrinfo hints, *servinfo;
+    std::memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    sin_size = sizeof client_addr;
+
+    if ((status = getaddrinfo(NULL, std::to_string(port).c_str(), &hints, &servinfo)) != 0)
+    {
+        std::cerr << "getaddrinfo error: " << gai_strerror(status) << std::endl;
+        exit(1);
+    }
+    if ((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1)
+        throw("server: socket");
+
+    // Bind socket
+    if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1)
+    {
+        close(sockfd);
+        throw("server: bind");
+    }
+    if (listen(sockfd, backlog) == -1)
+        throw("listen");
+    
+
+    freeaddrinfo(servinfo);
+}
+
+void Server::setup_threadpool()
+{
+    for(int i =0;i<pool_size;i++)
+        thread_pool.push_back(std::thread(&Server::threadpool_function,this));
+}
+
+void Server::accept_requests()
+{
+    sockaddr_in client_addr;
+    socklen_t client_length = sizeof(client_addr);
+
+    int newsockfd = accept(sockfd, (sockaddr *)&client_addr, &client_length);
+    if (newsockfd < 0)
+        throw("Error accepting connection");
+
+    // block for mutex
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex);
+        request_queue.push(newsockfd);
+    }
+    queue_cond.notify_one();
+}
+
+void Server::threadpool_function()
+{
+    while (true)
+    {
+        int work_sockfd;
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            queue_cond.wait(lock,[this](){return !request_queue.empty();});
+            work_sockfd = request_queue.front();
+            request_queue.pop();
+        }
+
+        Worker worker(work_sockfd);
+        worker.process_request();
+    }
+}
+
---
+++ Ver_3/Server.hpp
@@ -0,0 +1,41 @@
+#ifndef __Server_hpp__
+#define __Server_hpp__
+
+#include <iostream>
+#include <netdb.h>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <thread>
+#include "Worker.hpp"
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <fstream>
+
+class Server
+{
+    int port;
+    int sockfd;
+    sockaddr_in client_addr;
+    socklen_t sin_size;
+    int backlog;
+    std::vector<std::thread> thread_pool;
+    std::queue<int> request_queue;
+    int pool_size;
+    std::mutex queue_mutex;
+    std::condition_variable queue_cond;
+    
+
+    void setup_socket();
+    void threadpool_function();
+    void setup_threadpool();
+
+
+public:
+    Server(const char *port,const char *pool_size);
+    void accept_requests();
+};
+
+#endif
